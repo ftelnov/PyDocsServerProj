ebf10ec4c87d851c9756baa312d3a3d86a1725be
ftelnov <fntelnovfn@gmail.com>
2019-03-17 13:27:33 +0300

    Добавил наследование подключения скриптов и css-файлов. Добавил класс для будующей базы данных. Добавил обработчик 404

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import render_template, Flask, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    level = db.Column(db.String(120), default='NewBie')
+    experience = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return '<User id={} nickname={} email={} level={} exp={}>'.format(
+            self.id, self.nickname, self.email, self.level, self.experience)
 
 
 @app.route('/start', methods=['GET'])
@@ -11,6 +27,11 @@ def index():
 @app.route('/', methods=['GET'])
 def default():
     return redirect('/start')
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('not-found.html')
 
 
 if __name__ == '__main__':
