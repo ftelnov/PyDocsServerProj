68d370790634822801fb46a05ee8e80a5f1f2134
ftelnov <fntelnovfn@gmail.com>
2019-03-17 19:15:11 +0300

    Сделал механизм регистрации (пока только в python'е)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import render_template, Flask, redirect
+from flask import render_template, Flask, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,8 +8,9 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nickname = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     level = db.Column(db.String(120), default='NewBie')
     experience = db.Column(db.Integer, default=0)
@@ -34,5 +35,19 @@ def error_404(error):
     return render_template('not-found.html')
 
 
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        nickname = request.form.get('nickname')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User(nickname=nickname, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+    else:
+        return render_template('signup.html')
+
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(port=8080, host='127.0.0.1')
