dda4f9c972f168284741d63f74054d531eab467c
ftelnov <fntelnovfn@gmail.com>
2019-03-23 00:02:05 +0300

    Добавил комменты к каждой строке кода, начал работу с RESTful - структурой сервера, уже добавил получение данных пользователя в JSON формате, а также функцию преобразования User в JSON

--- ConstantsNFunctions.py
+++ ConstantsNFunctions.py
@@ -1,11 +1,12 @@
-from flask import render_template, Flask, redirect, request, make_response, session, flash
+from flask import render_template, Flask, redirect, request, make_response, session, flash, jsonify
+from flask_restful import reqparse, Api
 from flask_sqlalchemy import SQLAlchemy
 
-UPLOAD_FOLDER = 'static/users_uploads'
-ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = 'static/users_uploads'  # директория подгрузки изображений пользователей
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}  # доступные форматы подгрузки
 
-SECRET_KEY = 'anthrone'
-DATABASE_URI = 'sqlite:///main_database.db'
+SECRET_KEY = 'anthrone'  # секретка приложения
+DATABASE_URI = 'sqlite:///main_database.db'  # Uri бдшки
 TRACK_MODIFICATIONS = 'sqlite:///main_database.db'
 SESSION_TYPE = 'filesystem'
 
@@ -25,6 +26,7 @@ APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = TRACK_MODIFICATIONS
 APP.config['SESSION_TYPE'] = SESSION_TYPE
 APP.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 APP.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+Api(APP)
 
 # устанавливаем секретку
 APP.secret_key = SECRET_KEY
@@ -35,27 +37,49 @@ DB = SQLAlchemy(APP)
 
 # класс пользователя в базе данных
 class User(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
-    nickname = DB.Column(DB.String(120), unique=True, nullable=False)
-    password = DB.Column(DB.String(120), nullable=False)
-    email = DB.Column(DB.String(120), unique=True, nullable=False)
-    level = DB.Column(DB.String(120), default='Turtle')
-    experience = DB.Column(DB.Integer, default=0)
-    profile_image = DB.Column(DB.String(120), default='static/img/user/1.jpg')
+    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)  # уникальный идентификатор пользователя
+    nickname = DB.Column(DB.String(120), unique=True, nullable=False)  # имя пользователя
+    password = DB.Column(DB.String(120), nullable=False)  # пароль пользователя
+    email = DB.Column(DB.String(120), unique=True, nullable=False)  # email пользователя
+    level = DB.Column(DB.String(120), default='Turtle')  # текущий уровень пользователя
+    experience = DB.Column(DB.Integer, default=0)  # текущее кол-во опыта пользователя
+    profile_image = DB.Column(DB.String(120), default='static/img/user/1.jpg')  # путь к аве пользователя
 
-    def __repr__(self):
-        return '<User id={} nickname={} email={} level={} exp={}>'.format(
-            self.id, self.nickname, self.email, self.level, self.experience)
+
+def __repr__(self):
+    return '<User id={} nickname={} email={} level={} exp={}>'.format(
+        self.id, self.nickname, self.email, self.level, self.experience)
 
 
 # функция выдачи опыта игроку
 def give_exp(nickname, exp):
-    pre_experience = DB.session.query(User.experience).filter_by(
-        nickname=nickname).scalar()
+    # получаем пользователя из базы данных
+    user = DB.session.query(User).filter_by(
+        nickname=nickname).first()
+    # первичный опыт, т.е. который пользователь имеет в данный момент
+    pre_experience = user.experience
+    # добавляем опыт
     pre_experience += exp
+    # проходимся по уровням, ищем подходящий
     for level in LEVELS:
+        # если нашли
         if LEVELS[level][0] <= pre_experience <= LEVELS[level][1]:
-            DB.session.query.filter_by(nickname=nickname).update({'level': level, 'experience': pre_experience})
+            # обновляем опыт пользователя
+            user.update({'level': level, 'experience': pre_experience})
+            # подтверждаем изменения
             DB.session.commit()
+            # если нашли, возвращаем True
             return True
+    # если же не нашли, возвращаем False
     return False
+
+
+# функция трансформации пользователя из базы данных в словарь
+def user_to_dict(user):
+    return {
+        user['nickname']: user.nickname,  # никнейм
+        user['experience']: user.experience,  # опыт
+        user['level']: user.level,  # уровень
+        user['id']: user.id,  # персональный идентификатор
+        user['email']: user.email  # почта пользователя
+    }
--- main.py
+++ main.py
@@ -1,126 +1,168 @@
 import os
 
-from flask import url_for
 from shutil import copyfileobj
 from werkzeug.utils import secure_filename
 
 from ConstantsNFunctions import *
 
 
+# стартовая страничка
 @APP.route('/start', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
+# дефолтная страница
 @APP.route('/', methods=['GET'])
 def default():
     return redirect('/start')
 
 
+# обработчки 404-ошибки
 @APP.errorhandler(404)
 def error_404(error):
     return render_template('not-found.html')
 
 
-@APP.route('/profile', methods=['GET', 'POST', 'DELETE'])
+# обработчик окна профиля пользователя
+@APP.route('/profile', methods=['GET', 'POST'])
 def profile():
+    # обработчик метода get
     if request.method == 'GET':
+        # если пользователь залогинен
         if session.get('signin'):
             # получили пользователя, отфильтровав базу данных
             user = DB.session.query(User).filter_by(
                 nickname=session.get('nickname')).first()
-            identification = user.id
-            exp = user.experience
-            nickname = user.nickname
-            level = user.level
-            email = user.email
-            profile_image = user.profile_image
-            min_exp, max_exp = LEVELS[level]
+            # получили все параметры пользователя
+            identification = user.id  # id
+            exp = user.experience  # опыт
+            nickname = user.nickname  # имя
+            level = user.level  # уровень
+            email = user.email  # почта пользователя
+            profile_image = user.profile_image  # изображение профиля пользователя
+
+            min_exp, max_exp = LEVELS[level]  # максимальное и минимальное кол-во опыта текущего уровня пользователя
+
+            # расчитываем опыт и максимальный опыт на основе данных пользователя
             max_exp -= min_exp
             exp -= min_exp
             exp = exp / max_exp * 100
 
+            # отрисовываем окно пользователю
             return render_template('profile.html', nickname=nickname, exp=exp, level=level, id=identification,
                                    image_file=profile_image)
         else:
             return redirect('/signin')
 
+    # если же метод POST
     elif request.method == 'POST':
+        # если была нажата кнопка "Удалить аккаунт/Delete Account"
         if request.form.get('delete-account'):
+            # если пользователь залогинен
             if session['signin'] == 1:
+                # получаем пользователя, отфильтровав данные
                 user = User.query.filter_by(nickname=session['nickname'])
+                # удаляем изображение пользователя из папки
                 os.remove(user.profile_image)
+                # удаляем самого пользователя
                 user.delete()
+                # коммитим изменения
                 DB.session.commit()
+            # редиректим юзера на страничку входа
             return redirect('/signin')
 
+        # если была нажата кнопка "Выйти/Sign Out"
         elif request.form.get('sign-out'):
-            session['signin'] = 0
-            session['nickname'] = None
-            return redirect('/signin')
+            session['signin'] = 0  # помечаем, что не залогинен
+            session['nickname'] = None  # удаляем никнейм из настроек браузера
+            return redirect('/signin')  # редиректим пользователя на страничку входа
 
+        # если была нажата кнопка "Сохранить изображение/Save Photo"
         elif request.form.get('save-uploaded-image'):
+            # если изображения нет в файлах запроса
             if 'image' not in request.files:
+                # редиректим на текущую страницу
                 return redirect('/profile')
 
+            # получаем файл из запроса
             file = request.files['image']
+            # получаем его формат
             extension = file.filename.split('.')[-1]
 
+            # если файл существует и формат разрешен сервером
             if file and extension in ALLOWED_EXTENSIONS:
+                # получаем пользователя
                 user = DB.session.query(User).filter_by(nickname=session.get('nickname'))
+                # получаем расположение изображения пользователя
                 folder = APP.config[
                              'UPLOAD_FOLDER'] + '/images/' + user.first().nickname + '-profile-image.' + extension
-
+                # если изображение пользователя не стандартное
                 if user.first().profile_image != 'static/img/user/1.jpg':
+                    # удаляем его предидущее изображение
                     os.remove(user.first().profile_image)
+                # обновляем изображение пользователя
                 user.update({'profile_image': folder})
+                # коммитим изменения
                 DB.session.commit()
 
+                # сохраняем файл на сервере
                 with open(folder, 'wb+') as file_copy:
                     copyfileobj(file, file_copy)
-
+            # остаемся на текущей страничке
             return redirect('/profile')
 
 
+# обработчик входа в приложение
 @APP.route('/signin', methods=['POST', 'GET'])
 def signin():
+    # если GET запрос
     if request.method == 'GET':
+        # отрисовываем окно авторизации
         return render_template('signin.html')
+    # если же POST запрос
     elif request.method == 'POST':
-        nickname = request.form.get('nickname')
-        password = request.form.get('password')
+        nickname = request.form.get('nickname')  # получаем никнейм из формы
+        password = request.form.get('password')  # получаем пароль из формы
+        # если пользователь есть в базе данных
         if DB.session.query(User.id).filter_by(nickname=nickname, password=password).scalar():
-            session['signin'] = 1
-            session['nickname'] = nickname
-            return redirect('/profile')
+            # авторизовываем его в браузере
+            session['signin'] = 1  # флаг коннекта
+            session['nickname'] = nickname  # флаг никнейма
+            return redirect('/profile')  # редиректим его на окно профиля
         else:
             alert = '''<div class="alert alert-danger text-left mt-md-2 pd-1" role="alert">
                     This user does not exist!
-                    </div>'''
-            return render_template('signin.html', response=alert)
+                    </div>'''  # если же пользователя нет, отрисовываем предупреждение
+            return render_template('signin.html', response=alert)  # отрисовываем окно входа с предупреждением
 
 
+# обработчик регистрации
 @APP.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
         # получили все поля, в которые пользователь ввел какие-либо данные
-        nickname = request.form.get('nickname')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        password_submit = request.form.get('password_submit')
+        nickname = request.form.get('nickname')  # имя
+        email = request.form.get('email')  # почта
+        password = request.form.get('password')  # пароль
+        password_submit = request.form.get('password_submit')  # подтверждение пароля
 
         # прописал все условия в разных строчках, чтобы не загромождать код
+        # а существует ли пользователь
         conditional_1 = DB.session.query(User.id).filter_by(nickname=nickname).scalar()
+        # пароли совпадают?
         conditional_2 = password != password_submit
+        # проверяем, была ли такая почта уже зарегестрирована в базе данных
         conditional_3 = DB.session.query(User.id).filter_by(email=email).scalar()
 
         conditionals = []  # записываем все условия в массив строк
+        # строковые представления условий
         if conditional_1:
-            conditionals.append('<li> Такого никнейм уже занят! </li>')
+            conditionals.append('<li> Такого никнейм уже занят! </li>')  # invalid nickname
         if conditional_2:
-            conditionals.append('<li> Пароли не совпадают! </li>')
+            conditionals.append('<li> Пароли не совпадают! </li>')  # invalid password confirmation
         if conditional_3:
-            conditionals.append('<li> Почта уже привязана к другому аккаунту! </li>')
+            conditionals.append('<li> Почта уже привязана к другому аккаунту! </li>')  # invalid email
 
         # если что-либо пошло не так, возвращаемся на туже страничку, но уже с вписанной ошибкой
         response = '''
@@ -129,17 +171,24 @@ def signup():
                      {conditionals}
                 </ul>
         </div>'''.format(conditionals=' '.join(conditionals))
+        # если что-то пошло не так, возвращаемся с уже введенной ошибкой
         if conditionals:
             return render_template('signup.html', response=response)
 
+        # регаем пользователя
         user = User(nickname=nickname, email=email, password=password)
+        # добавляем пользователя в бд
         DB.session.add(user)
+        # коммитим изменения
         DB.session.commit()
 
-        session['nickname'] = nickname
-        session['signin'] = 1
+        # логинимся в браузере
+        session['nickname'] = nickname  # забиваем имя
+        session['signin'] = 1  # флаг коннекта
 
+        # редиректимся на профиль
         return redirect('/profile')
+    # если же GET запрос, просто отрисовываем окно регистрации
     elif request.method == 'GET':
         return render_template('signup.html')
 
@@ -157,6 +206,23 @@ def get_user(nickname):
     exp = exp / max_exp * 100
     return render_template('user.html', nickname=nickname, exp=exp, level=user.level,
                            image_file=user.profile_image)
+
+
+# Далее идут REST-обработчики
+# обрабатываем получение информации о пользователе
+@APP.route('/user/get_information', methods=['POST'])
+def get_user_information():
+    # парсим параметры POST-запроса
+    parser = reqparse.RequestParser()
+    # парсим никнэйм пользователя
+    parser.add_argument('nickname', required=True)
+    # вытаскиваем информацию о нем из базы данных
+    user = DB.session.query(User).filter_by(nickname=parser.parse_args('nickname')).first()
+    # если ничего не нашли, возвращаем соответствующий код
+    if not user:
+        return jsonify({'code': 404})
+    # возвращаем json-схему пользователя
+    return jsonify(user_to_dict(user))
 
 
 if __name__ == '__main__':
